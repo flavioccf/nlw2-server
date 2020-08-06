0ec006cf62aa98652379cadcefcd8fc8403ea40e
Flávio Carvalho Filho <flavioccf@hotmail.com>
2020-08-06 00:25:08 -0300

    finishing routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,19 @@
 import express from 'express';
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = express.Router();
+const classesController = new ClassesController();
+const connectionsController = new ConnectionsController();
 
 routes.get('/', (request, response) => {
     return response.json({ message: 'Hello World' });
 });
 
-routes.post('/classes', (request, response) => {
-    const data = request.body;
+routes.get('/classes', classesController.index);
+routes.post('/classes', classesController.create);
 
-    console.log(data);
-
-    return response.send();
-});
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
 
 export default routes;
